1ed2d269a249b54b3002296efd3f04112e524350
Brian McMillen <brian.mcmillen@explorelearning.com>
2020-05-10 19:11:22 -0400

    auto-detect the user's color scheme preference and update the theme accordingly

--- src/themes/ThemeSwitcher.vue
+++ src/themes/ThemeSwitcher.vue
@@ -7,10 +7,13 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import Theme from '../components/Theme.vue';
+import Theme from './Theme.vue';
 import ThxSelect from '../components/Select.vue';
 import { Component } from 'vue-property-decorator';
 import { ThemeTypeOptions } from './options';
+
+const darkMode = (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
+const defaultTheme = ThemeTypeOptions[darkMode ? 0 : 1];
 
 @Component({
   components: { Theme, ThxSelect }
@@ -18,7 +21,22 @@ import { ThemeTypeOptions } from './options';
 export default class ThemeSwitcher extends Vue {
   css = 'thx-theme-switcher'
   themeOptions = ThemeTypeOptions
-  theme = ThemeTypeOptions[0]
+  theme = defaultTheme
+
+  onColorSchemeChange(e: { matches: boolean }) {
+    const darkMode = !!e.matches;
+    this.theme = ThemeTypeOptions[darkMode ? 0 : 1];
+  }
+
+  created() {
+    window.matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener('change', this.onColorSchemeChange);
+  }
+
+  destroyed() {
+    window.matchMedia('(prefers-color-scheme: dark)')
+      .removeEventListener('change', this.onColorSchemeChange);
+  }
 }
 </script>
 
